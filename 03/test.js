8371fdfbcfb7888abcc1eb64808ff70eaa490fe1
aravergar <aravergar@gmail.com>
2016-10-05 23:56:06 +0200

    Start chapter 3
    Make a test file to test some code from chapter 3

---
+++ 03/test.js
@@ -0,0 +1,12 @@
+//var <function_name> = function(<function_arguments>){
+//    <function_body>
+//};
+
+var power = function(base, exponent){
+    var result = 1;
+    for(var count=0; count<exponent; count++)
+        result*=base;
+    return result;
+};
+
+console.log(power(2,10));
