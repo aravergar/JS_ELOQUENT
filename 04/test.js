50fb621f8e5207a34d7dfb49601170bd621be6d2
aravergar <aravergar@gmail.com>
2016-10-06 20:22:19 +0200

    Start chapter 4
    Make some tests on chapter 4

---
+++ 04/test.js
@@ -0,0 +1,13 @@
+var foo = "foo";
+console.log(typeof foo.toUpperCase);
+console.log(typeof foo.toUpperCase());
+console.log(foo.toUpperCase());
+
+var lotr = [];
+console.log(lotr.push("Lord", "of", "the", "Rings"));
+console.log(lotr);
+console.log(lotr.join("_"));
+console.log(lotr.pop());
+console.log(lotr.push("Rings"));
+console.log(lotr[3]);
+console.log(lotr.join(""));
