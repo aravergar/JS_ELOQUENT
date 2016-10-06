17e4b71b52f48cbbaf94116cfa2fee59af5c145e
aravergar <aravergar@gmail.com>
2016-10-07 01:03:48 +0200

    End exercise 2 chapter 4

--- 04/exercise_01.js
+++ 04/exercise_01.js
@@ -1,12 +1,17 @@
 function range(start, end, step){
 //    if (step == undefined)
 //        step = 1;
-    if (arguments.length != 3)
+    if (arguments.length != 3 || step == 0)
         step = 1;
     result = [];
-    for(i=start; i<=end; i+=step){
-        result.push(i);
-    }
+    if(step>0)
+        for(i=start; i<=end; i+=step){
+            result.push(i);
+        }
+    else
+        for(i=start; i>=end; i+=step){
+            result.push(i);
+        }
     return result;
 }
 
@@ -20,3 +25,4 @@ function sum(array){
 
 console.log(range(1,10,2));
 console.log(sum(range(1,10,2)));
+console.log(range(5,2,-1));
