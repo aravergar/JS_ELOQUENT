269d07eed668737fa63e16cfbf5bdd2b9b188088
aravergar <aravergar@gmail.com>
2016-10-06 21:22:12 +0200

    Exercise 1 on chapter 4
    Start making exercise 1

---
+++ 04/exercise_01.js
@@ -0,0 +1,22 @@
+function range(start, end, step){
+//    if (step == undefined)
+//        step = 1;
+    if (arguments.length != 3)
+        step = 1;
+    result = [];
+    for(i=start; i<=end; i+=step){
+        result.push(i);
+    }
+    return result;
+}
+
+function sum(array){
+    result = 0;
+    for(i=0; i<array.length; i++){
+        result+=array[i];
+    }
+    return result;
+}
+
+console.log(range(1,10,2));
+console.log(sum(range(1,10,2)));
